31734f7f172a8d0553387d8b7086e90140196f8a
Guillaume <guillaume.dupuy@ynov.com>
2022-02-23 23:11:31 +0100

    fix data & reload img & add filter'

--- api/appMongo.py
+++ api/appMongo.py
@@ -26,6 +26,84 @@ def home_page():
     return render_template("index.html",
         cards=cards)
 
+@app.route('/post_filtre/', methods=['GET', 'POST'])
+def post_filtre(): 
+    return render_template('post_filtre.html')
+
+@app.route('/affich_filtre/', methods=['GET', 'POST'])
+def affich_filtre():  
+    cards = mongo.db.cards.find({})
+    df = pd.DataFrame.from_records(cards)
+    df = df.drop(["_id"], axis=1)
+    cards_dict = df.to_dict()
+
+    nom = []   
+    for values in cards_dict['Nom'].items():
+        nom.append(values) 
+        # print(values)
+
+      
+    for key,values in cards_dict.items():
+        # rang.append(values) 
+        print(key)
+
+    rang = []
+    for values in cards_dict['Rang'].items():
+        rang.append(values) 
+        # print(values) 
+
+    note = []
+    for values in cards_dict['Notation /100'].items():
+        note.append(values) 
+        # print(values) 
+
+    use = []
+    for values in cards_dict['Utilisation %'].items():
+        use.append(values) 
+        # print(values) 
+
+    vic = []
+    for values in cards_dict['Victoire %'].items():
+        vic.append(values) 
+        # print(values)
+    
+    img = []
+    for values in cards_dict['Image'].items():
+        img.append(values) 
+        # print(values) 
+
+    # print(nom[int(request.form['number'])][1])
+
+    # print(cards_dict['Nom'][0])
+    # print(str(request.form['input']))
+    # print(str(request.form['number']))
+
+    # filtre = filter_set(cards_dict['Nom'], str(request.form['number']))
+    # filtre = filter_set(test[int(request.form['number'])][1],str(request.form['input']))
+
+    name = nom[int(request.form['number'])][1]
+    rank = rang[int(request.form['number'])][1]
+    notes = note[int(request.form['number'])][1]
+    uses = use[int(request.form['number'])][1]
+    win = vic[int(request.form['number'])][1]
+    imgs = img[int(request.form['number'])][1]
+
+    # print(filtre)
+    # filtre = list(filtre)
+    # print(filtre)
+
+    return render_template('affich_filtre.html',name=name,rank=rank,notes=notes,uses=uses,win=win,imgs=imgs)
+
+def filter_set(cards_dict, search_string):
+    # print(cards_dict)
+    # print(search_string)
+    def iterator_func(x):
+        print(x)
+        if search_string in str(x):
+            return True
+        return False
+    return filter(iterator_func, cards_dict)
+
 @app.route('/dashboard/')
 def dashboard():
     cards = mongo.db.cards.find({})
@@ -40,7 +118,7 @@ def upload():
     return render_template('upload.html')
 
 # Get the uploaded files
-@app.route("/upload/", methods=['POST'])
+@app.route("/upload/", methods=['GET', 'POST'])
 def uploadFiles():
       # get the uploaded file
       uploaded_file = request.files['file']
@@ -95,7 +173,6 @@ def mongoimport(file_path,coll_name='cards', db_name='scraping2',db_url='localho
 
 def path_to_image_html(path):
     return '<img src="'+ path + '" width="60" >'
-
 
 if __name__ == "__main__":
     app.run(debug="mongodb://localhost:27017/scraping")
